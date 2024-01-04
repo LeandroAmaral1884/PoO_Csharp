58e09704ca1a351c4027c798233719bb86049599
leandro.amaral <amaral4957@gmail.com>
2024-01-03 21:09:33 -0300

    Herança de Construtores

---
+++ Heranca_Construtores/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("\n-----------------Classe Pessoa\n");
+Pessoa pessoa1 = new Pessoa();
+
+Console.WriteLine("\n----------------Classe Aluno herda Pessoa\n");
+Aluno aluno1 = new Aluno();
+
+Console.WriteLine("\n---------------Classe Aluno herda Pessoa passando parametro\n");
+Aluno aluno2 = new Aluno("Leandro");
+Console.ReadLine();
+
+
+//classe base
+class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("construtor da classe Pessoa");
+    }
+    public Pessoa( string nome)
+    {
+        Console.WriteLine("construtor da classe Pessoa com parametro");
+    }
+}
+
+class Aluno : Pessoa
+{
+    public Aluno() : base() // base() chama construtor  public Pessoa() sem parametro
+    {
+        Console.WriteLine("construtor da classe Aluno");
+    }
+    public Aluno(string nome) :base(nome) // base(nome) chama construtor   public Pessoa( string nome) com parametro
+    {
+        Console.WriteLine("construtor da classe Aluno com parametro");
+    }
+}
---
+++ Heranca_Objeto/Program.cs
@@ -0,0 +1,13 @@
+﻿Nada nada = new();
+Console.WriteLine("Estão herdando da Classe Object\nNada nada = new();\nnada.Equals(nada);\r\nnada.ToString();\r\nnada.GetType();\r\nnada.GetHashCode();");
+nada.Equals(nada);
+nada.ToString();
+nada.GetType();
+nada.GetHashCode();
+
+Console.ReadLine();
+
+class Nada()
+{
+
+}
